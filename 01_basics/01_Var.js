831feaf14b9ef659dbf41696937cb10bfe6dc752
Swatantra-hub <bharatramji@gmail.com>
2024-11-30 09:39:43 +0000

    let,var,const

---
+++ 01_basics/01_Var.js
@@ -0,0 +1,5 @@
+const accountId="112233"
+let accountPassword="bruhhh"
+var accountEmail="bharatramji@gmail.com"
+accountCity="Awb"
+console.table([accountId, accountEmail, accountPassword , accountCity])
